32b6f5584fffacc40f1f5b8356a4e5fcd41aa939
Federico Trombetti <ftrombetti@hotmail.it>
2019-04-09 12:21:25 +0200

    Improved performance. Jotaro dodge this

--- source/interface/main.js
+++ source/interface/main.js
@@ -24,32 +24,39 @@ setTimeout(function() {
 
 	let faceCascadeFile = './haarcascade_frontalface_default.xml';
 	let classifier = new cv.CascadeClassifier();
+	let dst = new cv.Mat(640, 480, cv.CV_8UC4);
+
+
+
+	let faces = new cv.RectVector();
+	let src = new cv.imread('videoInput');
+	let gray = new cv.Mat();
+	let FPS = 30;
 
 	var loopFun =  function() {
+
+		let begin = Date.now()
+
 	
-		let src = cv.imread('videoInput');
-		let gray = new cv.Mat();
-		cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
-		let faces = new cv.RectVector();
-		let msize = new cv.Size(0, 0);
+		src = new cv.imread('videoInput');
+		src.copyTo(dst);
 
+		cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
 
-		classifier.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+		classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
 
 		for (let i = 0; i < faces.size(); ++i) {
-		    let roiGray = gray.roi(faces.get(i));
-		    let roiSrc = src.roi(faces.get(i));
 		    let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
 		    let point2 = new cv.Point(faces.get(i).x + faces.get(i).width,
 		                              faces.get(i).y + faces.get(i).height);
-		    cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
-		    roiGray.delete(); roiSrc.delete();
+		    cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
 		}
 
-		cv.imshow('videoOutput', src);
-		src.delete(); gray.delete(); faces.delete();
+		cv.imshow('videoOutput', dst);
+
 	
-		setTimeout(loopFun, 1000/60);
+		setTimeout(loopFun, 1000/FPS - Date.now() + begin);
+
 
 	}
 
